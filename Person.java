b250daa63f1cea5e47edcc09371ce699556a9cb3
Damon <nichold5@mail.uc.edu>
2020-11-17 20:01:33 +0000

    Initial commit

---
+++ Person.java
@@ -0,0 +1,34 @@
+class Person {
+  String name;
+  String color;
+  int age;
+
+
+    
+
+    Person(String uName, int uAge, String uColor) {
+
+        name = uName;
+      
+        age = uAge;
+
+       color = uColor;
+    }
+
+    String getName(){
+      return name;
+  
+    }
+
+    int getAge() {
+      return age;
+
+    }
+ 
+
+    String getColor() {
+      return color;
+    }
+
+
+    }
